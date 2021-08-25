23c1d1800c4060bbbdcc768ea50ed1906e1dfa20
NoahKirchner <noahthomkirchner@gmail.com>
2021-08-25 19:46:52 +0000

    Lord have mercy on my eternal soul for the sins I have committed in the name of progress. Grabs IPs and formats them into a list of lists. @TODO dictionary them.

--- scraperotation.py
+++ scraperotation.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+import base64
 
 class Rotator:
     proxy_source    = None
@@ -9,16 +11,71 @@ class Rotator:
 
     def __init__(self):
         self.proxy_source = "http://free-proxy.cz/en/proxylist/country/all/all/ping/level1{}"
+        self._clean_proxies(self.return_raw_proxies(1))
 
-    def return_proxies(self, pages:int=1):
-        content_list = list()
+    def return_raw_proxies(self, pages:int=1):
+        encode_regex    = re.compile(r'".+"')
+        content_list    = list()
+        raw_list        = list()
         for i in range(1,pages+1):
             content_list.append(requests.get(self.proxy_source.format(i)))
         for i in content_list:
             soup = BeautifulSoup(i.content, 'html.parser')
             proxy_list = soup.find(id='proxy_list').findAll('td')
             for td in proxy_list:
-                print(td.text)
+                if td.find('script'):
+                    for line in td.find('script'):
+                        base64_string = encode_regex.search(line).group().replace('"', '')
+                        raw_list.append([base64.b64decode(base64_string.encode('ascii')).decode('ascii')])
+                        #Changing this type to list so that we can combine this data into individual tuples
+                        #later. Right now it is just one big stream of data, but changing the type of the
+                        #first element allows us to easily split the data up.
+
+                line = td.text
+                if line and line[0] is " ":
+                    line = line.replace(" ", "", 1)
+                if len(line) > 1:
+                    raw_list.append(line)
+        return raw_list
 
 
-Test = Rotator().return_proxies(1)
+
+    def _clean_proxies(self, raw_list:list):
+        buffer = list()
+        out_list = list()
+
+        for item in raw_list:
+            if buffer and type(item) is list:
+                out_list.append(list(buffer))
+                buffer = list()
+                buffer.append(item)
+            else:
+                buffer.append(item)
+
+        for i, item in enumerate(out_list):
+            item[0] = item[0][0]
+
+        for item in out_list:
+            if len(item) != 11:
+                out_list.remove(item)
+
+
+
+
+
+
+
+
+
+        print(out_list)
+        return out_list
+
+
+
+
+
+
+
+
+
+Test = Rotator()
